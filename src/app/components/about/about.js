96651596f0db4ce19270d3bf8336eefeea86c02e
Konrad Dunski <44006045+k-dun@users.noreply.github.com>
2024-04-11 00:03:06 +0100

    Adapted About for medium screen

--- src/app/components/about/about.js
+++ src/app/components/about/about.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function About() {
   return (
     <>
-      <div className="w-full min-h-[100svh] flex flex-col bg-[#5AC9F4] pt-14">
+      <div className="md:hidden w-full min-h-[100svh] flex flex-col bg-[#5AC9F4] pt-14">
         <h2 className="mx-auto text-[#FCFAFF] font-marker text-4xl" id="about">ABOUT</h2>
         <h3 className="mx-6 md:mx-36 pt-8 text-[#404040] font-dmsans text-lg">Lorem ipsum dolor sit amet, 
           consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
@@ -17,6 +17,22 @@ export default function About() {
           mollit anim id est laborum.</h3>
         <div className="w-full pt-12 md:justify-end">
           <Image src="/bussin.png" className="w-full md:w-[80vw]" width="2000" height="520" alt="bussin by SomeHoodlum" />
+        </div>
+      </div>
+
+      <div className="md:w-full md:min-h-[100vh] md:flex md:flex-col md:items-center md:bg-[#5ac9f4] md:pt-14">
+        <h2 className="md:mx-auto md:text-[#FCFAFF] md:font-marker md:text-6xl" id="about">ABOUT</h2>
+        <h3 className="md:w-[70vw] md:pt-12 md:text-[#404040] md:font-dmsans md:text-lg">Lorem ipsum dolor sit amet, 
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+          consequat. <br /><br />
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+          mollit anim id est laborum. <br /><br />
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+          mollit anim id est laborum.</h3>
+        <div className="md:w-full md:pt-12 md:flex md:justify-center">
+          <Image src="/bussin.png" className="md:w-[70vw] md:border-l md:border-r md:border-l-[#FCFAFF]" width="2000" height="520" alt="bussin by SomeHoodlum" />
         </div>
       </div>
     </>
